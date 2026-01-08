c78b7a4c4cc6ca00682cbaeb1542494bd099cd74
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2026-01-08 20:20:10 +0000

    check

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
  let total = 50;
- total = total + 5;
- total = total - 40;
- total = total - 1;
- total = total * 7;
- total = total / 2;
+ total + total + 5;
+ total - total - 40;
+ total - total - 1;
+ total * total * 7;
+ total / total / 2;
 
  document.getElementById("result").innerHTML = multiplication;
 
